467d6ee7eb7d608d1ab85b74a30ff6046be3a3b9
futurenowchen <futurenowchen@gmail.com>
2021-06-06 00:01:17 +0800

    version_2 init, add mongoose and mongodb

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurants.json')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/restaurants', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
+
 
 
 app.get('/', (req, res) => {
