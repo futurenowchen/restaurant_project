bf13a45cb94baccd897940caf735dbcc865f4ae5
futurenowchen <futurenowchen@gmail.com>
2021-06-06 20:44:59 +0800

    add create restaurant function

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const restaurantList = require('./models/restaurants.js')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 mongoose.connect('mongodb://localhost/restaurants', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
@@ -17,6 +18,7 @@ db.once('open', () => {
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 
@@ -27,11 +29,35 @@ app.get('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
+//為什麼get('/restaurants/new')，放在get('/restaurants/:restaurant_id')後面，會造成頁面將new當id讀入資料庫中，因而錯誤呢？
+
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const id = req.params.restaurant_id
   return restaurantList.findById(id)
     .lean()
     .then((restaurant) => res.render('show', { restaurant: restaurant }))
+    .catch(error => console.log(error))
+})
+
+app.post('/restaurants', (req, res) => {
+  const restaurant = req.body
+  return restaurantList.create({
+    id: restaurant.id,
+    name: restaurant.name,
+    name_en: restaurant.name_en,
+    category: restaurant.category,
+    image: restaurant.image,
+    location: restaurant.location,
+    phone: restaurant.phone,
+    google_map: restaurant.google_map,
+    rating: restaurant.rating,
+    description: restaurant.description
+  })
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
@@ -42,6 +68,7 @@ app.get('/search', (req, res) => {
   })
   res.render('index', { restaurants: restaurants, keyword: keyword })
 })
+
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
