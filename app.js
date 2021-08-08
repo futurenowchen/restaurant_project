09b6c1607e091749fd45eaad64bdc3ae802fc90d
futurenowchen <futurenowchen@gmail.com>
2021-08-08 21:39:33 +0800

    feat: add express-seesion

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
@@ -12,6 +13,13 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(routes)
 
 app.listen(port, () => {
